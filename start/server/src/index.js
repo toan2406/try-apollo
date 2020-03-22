8bea787282d1c29fd37526c3050efc8d4c95eee4
Toan Nguyen <toan.nguyen@employmenthero.com>
2020-03-22 11:58:22 +0700

    add auth flow

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer } = require('apollo-server');
+const isEmail = require('isemail');
 const typeDefs = require('./schema');
 const { createStore } = require('./utils');
 const resolvers = require('./resolvers');
@@ -9,6 +10,14 @@ const UserAPI = require('./datasources/user');
 const store = createStore();
 
 const server = new ApolloServer({
+  context: async ({ req }) => {
+    const auth = (req.headers && req.headers.authorization) || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
+    if (!isEmail.validate(email)) return { user: null };
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = (users && users[0]) || null;
+    return { user: { ...user.dataValues, authorizationToken: auth } };
+  },
   typeDefs,
   resolvers,
   dataSources: () => ({
